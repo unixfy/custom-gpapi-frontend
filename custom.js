c0977dc11dbb66a19e213a54ce1144eef4e4b60c
jwhitefield42 <71399414+jwhitefield42@users.noreply.github.com>
2020-09-16 10:19:43 -0700

    Fix typo

--- custom.js
+++ custom.js
@@ -129,7 +129,7 @@ function appendData(data) {
             tr.innerHTML +=`
                 <td>${switcherData["BetAmounts"]}</td>
                 <td data-order="${switcherData["ProfitPerCardRaw"]}" class="ProfitPerCard">${switcherData["ProfitPerCard"]}</td>
-                <td><a target="_blank" rel="noreferrer" style=background-color:#E1DFDB" class="btn btn-block" href=${data[i].PlayUrl}>Go</a></td>    
+                <td><a target="_blank" rel="noreferrer" style="background-color:#E1DFDB" class="btn btn-block" href=${data[i].PlayUrl}>Go</a></td>    
             `
 
             // Listener to update table when bet method dropdown is switched
