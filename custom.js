39a3c5d875fd545d537d23997013118b5392b4cd
jwhitefield42 <71399414+jwhitefield42@users.noreply.github.com>
2021-02-01 08:25:45 -0800

    Reduce discount to 5%

--- custom.js
+++ custom.js
@@ -1,5 +1,5 @@
 // Edit this if SB discount changes. Needs to be in decimal format
-let gambitDiscountPercent = 0.10;
+let gambitDiscountPercent = 0.5;
 
 // Pull data on page load
 window.onload = function() {
